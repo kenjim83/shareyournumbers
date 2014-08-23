14f784cdb42b0a82fa6363e60506e924ab36373e
Kenji Miwa <kenjim83@gmail.com>
2014-08-22 21:49:23 -0700

    went back to working state before ActiveAdmin install

--- Gemfile
+++ Gemfile
@@ -8,7 +8,10 @@ gem 'pg'
 
 # Image uploading
 gem 'carrierwave'
-gem 'rmagick', '2.13.2'
+gem 'mini_magick'
+
+# Active Admin
+gem 'activeadmin', github: 'gregbell/active_admin'
 
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
--- app/uploaders/poster_uploader.rb
+++ app/uploaders/poster_uploader.rb
@@ -3,8 +3,8 @@
 class PosterUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  # include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -33,7 +33,7 @@ class PosterUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [50, 50]
+    process :resize_to_fit => [50, 75]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
